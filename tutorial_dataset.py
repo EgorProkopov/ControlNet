7923587fc273522a01d7037c37af8151b8e16c3f
Egor Prokopov <egorprokopov216@gmail.com>
2024-09-27 20:46:38 +0300

    update for mps

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -4,6 +4,8 @@ import os
 import os.path as osp
 import numpy as np
 
+import albumentations as albu
+
 from torch.utils.data import Dataset
 from annotator.util import resize_image, HWC3
 from annotator.canny import CannyDetector
@@ -18,6 +20,14 @@ class MyDataset(Dataset):
         self.masksdir = masks_dir
         self.data = os.listdir(self.datadir)
 
+        self.transform = albu.Compose([
+            albu.HorizontalFlip(p=0.3),
+            albu.VerticalFlip(p=0.3),
+            albu.RandomCrop(
+                np.random.randint(self.width, 800), np.random.randint(self.height, 600), p=0.8
+            ),
+        ])
+
     def __len__(self):
         return len(self.data)
 
@@ -27,11 +37,12 @@ class MyDataset(Dataset):
 
         image = cv2.imread(osp.join(self.datadir, item))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, (self.width, self.height))
-        image = resize_image(image, self.width)
-
         mask = cv2.imread(osp.join(self.masksdir, item))
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+
+        image = cv2.resize(image, (self.width, self.height))
+        image = resize_image(image, self.width)
+        mask = cv2.resize(mask, (self.width, self.height))
         mask = resize_image(mask, self.width)
 
         mask = mask.astype(np.float32) / 255.0
